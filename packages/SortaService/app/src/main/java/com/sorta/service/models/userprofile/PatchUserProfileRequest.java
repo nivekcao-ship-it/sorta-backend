0f16eac6f1b342763f7db7f571b5539d0a9c37d4
Nivek Cao <nivekcao@amazon.com>
2025-08-05 10:21:36 +1000

    Update patch user request to ignore optional fields

--- packages/SortaService/app/src/main/java/com/sorta/service/models/userprofile/PatchUserProfileRequest.java
+++ packages/SortaService/app/src/main/java/com/sorta/service/models/userprofile/PatchUserProfileRequest.java
@@ -1,10 +1,14 @@
 package com.sorta.service.models.userprofile;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import java.util.List;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PatchUserProfileRequest {
+    @JsonProperty(required = true)
     private String userId;
     private List<UpdateRoomInfoRequest> roomInfo;
 
